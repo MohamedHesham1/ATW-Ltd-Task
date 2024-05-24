a3301c62bb64fbe7c84d32212f9cebc3cde4c156
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2024-05-25 02:21:47 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavbarComponent from './components/NavbarComponent.vue'
+</script>
+
+<template>
+  <header>
+    <NavbarComponent />
+  </header>
+
+  <main>hello</main>
+</template>
+
+<style scoped></style>
